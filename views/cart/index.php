4dec71223258bb2af1f79640ef51df0dd7905757
marie <marie.sochala@gmail.com>
2016-06-02 17:36:10 +0200

    Création de la boucle pour insertion des données de l'achat

--- views/cart/index.php
+++ views/cart/index.php
@@ -5,17 +5,34 @@
 		<div class="column">
 			<h1 class="ui container">Mon Panier</h1>
 			<div class="ui list">
+				<h4 class="item header">Montant total : ??? &euro;</h4>
+				<h4 classs="item header">Nombre produits : ???</h4>
 				<div class="item">
 					<a href="/cart/checkout" class="ui button">Finaliser ma commande</a>
 				</div>
-				<h4 class="item header">Montant total : ??? &euro;</h4>
-				<h4 classs="item header">Nombre produits : ???</h4>
 			</div>
 		</div>
 		<div class="column">
 			<table class="ui table compact">
+				<thead>
+					<tr>
+						<th>Picture</th>
+						<th>Name</th>
+						<th>Price</th>
+					</tr>
+				</thead>
 				<!-- une boucle ! -->
-			</table>
+				<?php foreach($products as $product): ?>
+				<tbody>
+					<tr>
+						<td><?=$product->name?></td>
+						<td><img src="<?=$product->picture?>" width="50px" height="50px"></td>
+						<td><?=$product->price?></td>
+					</tr>
+				</tbody>
+			<?php endforeach; ?>
+
+ 			</table>
 		</div>
 	</div>
 
