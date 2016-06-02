bc64083d34405ef1f92c94491887acda856e0b8d
marie <marie.sochala@gmail.com>
2016-06-02 08:30:29 +0200

    Affichage des produits de la boutique + bouton acheter

--- views/home.php
+++ views/home.php
@@ -5,5 +5,23 @@
 	<hr class="ui divider">
 	<section class="ui three stackable cards">
 		<!-- boucle des produits ui card -->
+		<?php foreach ($products as $product) : ?>
+ 			<div class="ui card">
+ 				<div class="visuel">
+ 					<img src="<?=$product->picture?>">
+ 				</div>
+ 				<div class="description">
+ 					<div class="content">
+ 						<?php=$product['name']?>
+ 					</div>
+ 				</div>
+ 				<div class="achat">
+ 					<form method="POST" action="/cart/add" name="commande">
+ 						<input type="hidden" value="<?=$product->id?>" name="id"/>
+ 						<button class="ui button" type="submit">Acheter</button>
+ 					</form>
+ 				</div>
+ 			</div>
+ 		<?php endforeach;?>
 	</section>
 </div>
