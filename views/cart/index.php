f702b6ae36d7064f62fc60e4c7a7d50b008322d6
marie <marie.sochala@gmail.com>
2016-06-06 11:56:00 +0200

    Affichage montant du panier et nbre d'achat

--- views/cart/index.php
+++ views/cart/index.php
@@ -5,8 +5,8 @@
 		<div class="column">
 			<h1 class="ui container">Mon Panier</h1>
 			<div class="ui list">
-				<h4 class="item header">Montant total : ??? &euro;</h4>
-				<h4 classs="item header">Nombre produits : ???</h4>
+				<h4 class="item header">Montant total : <?=Cart::total($products)?> &euro;</h4>
+				<h4 classs="item header">Nombre produits : <?=Cart::count($products)?> </h4>
 				<div class="item">
 					<a href="/cart/checkout" class="ui button">Finaliser ma commande</a>
 				</div>
