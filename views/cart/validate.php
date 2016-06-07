d41dd90b2e75c576cdc39766361512c7f910911d
marie <marie.sochala@gmail.com>
2016-06-07 15:50:38 +0200

    Ajout confirmation de la validation d'achat

---
+++ views/cart/validate.php
@@ -0,0 +1,8 @@
+<?php $this->layout('layout', ['title' => 'Super boutique']) ?>
+<h1>Etes vous sûr(e) de vouloir annuler votre commande</h1>
+	<div class="item">
+        <a href="/logout" class="ui button">Oui</a>
+    </div>
+    <div class="item">
+        <a href="/cart/checkout" class="ui button">Non</a>
+    </div>
